aaa1934b24cc3eb303490fe7a5d188fb097ebe36
jswiss <swissjoshua@gmail.com>
2021-06-14 09:50:57 +0100

    opt for global types

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,24 @@
 /// <reference types="@sveltejs/kit" />
+
+interface Nullable<T> {
+	T: T | null;
+}
+
+interface Status {
+	status: number;
+}
+
+interface Metadata {
+	layout: string;
+	title: string;
+	summary?: string;
+	date: string;
+	tags: string;
+	published: boolean;
+}
+interface Post {
+	html: string;
+	metadata: Metadata;
+	filename: string;
+	date?: number;
+}
